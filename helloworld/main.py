f7a8e9137e1645478bb62b71562f1214eec53a34
jzoubian <julienzoubian@gmail.com>
2018-06-01 16:40:50 +0200

    Update main.py
    python 3

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,9 +1,10 @@
 import sys
+from __future__ import print_function
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    print "Hello, world"
+    print("Hello, world")
 
     return 0
